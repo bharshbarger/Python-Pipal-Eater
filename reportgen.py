1af76d182db2fc7ff781e4c38245136c089987e5
bharshbarger <bill.harshbarger@gmail.com>
2018-06-01 15:28:14 -0500

    fix table font and appearance issues but not width issue

--- reportgen.py
+++ reportgen.py
@@ -50,6 +50,10 @@ class Reportgen(object):
         total_table = self.document.add_table(rows=0, cols=1)
         #one line per cell
         total_table.style = 'Table Grid'
+        #set table font
+        font = total_table.style.font
+        font.name = 'Hind'
+        font.size = Pt(11)
         #try to do autofit, doesnt seem to be working in libreoffice, but autofit is default anyway....
         #possibly related https://github.com/python-openxml/python-docx/issues/209
         #total_table.allow_autofit
@@ -57,11 +61,21 @@ class Reportgen(object):
         cells[0].text = str(total)
         cells = total_table.add_row().cells
         cells[0].text = str(unique)
+        #hack to fix width? https://github.com/python-openxml/python-docx/issues/315
+        for r in total_table.rows:
+            for c in r._tr.tc_lst:
+                tcW = c.tcPr.tcW
+                tcW.type = 'auto'
+                tcW.w = 0
         paragraph = self.document.add_paragraph()
 
         #second table - top 10 overall pws
         top_10_table = self.document.add_table(rows=0, cols=1)
         top_10_table.style = 'Table Grid'
+        #set table font
+        font = top_10_table.style.font
+        font.name = 'Hind'
+        font.size = Pt(11)
         for i, val in enumerate(top_10):
             cells = top_10_table.add_row().cells
             cells[0].text = str(top_10[i])
@@ -70,6 +84,10 @@ class Reportgen(object):
         #third table - top 10 overall base dict words
         top_10_base_table = self.document.add_table(rows=0, cols=1)
         top_10_base_table.style = 'Table Grid'
+        #set table font
+        font = top_10_base_table.style.font
+        font.name = 'Hind'
+        font.size = Pt(11)
         for i, val in enumerate(top_10_base):
             cells = top_10_base_table.add_row().cells
             cells[0].text = str(top_10_base[i])
@@ -78,6 +96,10 @@ class Reportgen(object):
         #fourth table - passwords by length
         length_order_table = self.document.add_table(rows=0, cols=1)
         length_order_table.style = 'Table Grid'
+        #set table font
+        font = length_order_table.style.font
+        font.name = 'Hind'
+        font.size = Pt(11)
         for i, val in enumerate(lengths):
             cells = length_order_table.add_row().cells
             cells[0].text = str(lengths[i])
@@ -86,6 +108,10 @@ class Reportgen(object):
         #fifth table - passwords by count 
         count_order_table = self.document.add_table(rows=0, cols=1)
         count_order_table.style = 'Table Grid'
+        #set table font
+        font = count_order_table.style.font
+        font.name = 'Hind'
+        font.size = Pt(11)
         for i, val in enumerate(counts):
             cells = count_order_table.add_row().cells
             cells[0].text = str(counts[i])
@@ -94,6 +120,10 @@ class Reportgen(object):
         #sixth table
         one_to_six_table = self.document.add_table(rows=0, cols=1)
         one_to_six_table.style = 'Table Grid'
+        #set table font
+        font = one_to_six_table.style.font
+        font.name = 'Hind'
+        font.size = Pt(11)
         for i, val in enumerate(one_to_six):
             cells = one_to_six_table.add_row().cells
             cells[0].text = str(one_to_six[i])
@@ -102,6 +132,10 @@ class Reportgen(object):
         #seventh table - the trailing number
         trailing_number_table = self.document.add_table(rows=0, cols=1)
         trailing_number_table.style = 'Table Grid'
+        #set table font
+        font = trailing_number_table.style.font
+        font.name = 'Hind'
+        font.size = Pt(11)
         for i, val in enumerate(trailing_number):
             cells = trailing_number_table.add_row().cells
             cells[0].text = str(trailing_number[i])
@@ -110,6 +144,10 @@ class Reportgen(object):
         #eigth table - last digit in pw
         last_1digit_table = self.document.add_table(rows=0, cols=1)
         last_1digit_table.style = 'Table Grid'
+        #set table font
+        font = last_1digit_table.style.font
+        font.name = 'Hind'
+        font.size = Pt(11)
         for i, val in enumerate(last_1digit):
             cells = last_1digit_table.add_row().cells
             cells[0].text = str(last_1digit[i])
@@ -118,6 +156,10 @@ class Reportgen(object):
         #ninth table - last 2 digits of pw
         last_2digit_table = self.document.add_table(rows=0, cols=1)
         last_2digit_table.style = 'Table Grid'
+        #set table font
+        font = last_2digit_table.style.font
+        font.name = 'Hind'
+        font.size = Pt(11)
         for i, val in enumerate(last_2digit):
             cells = last_2digit_table.add_row().cells
             cells[0].text = str(last_2digit[i])
@@ -126,6 +168,10 @@ class Reportgen(object):
         #tenth table - last 3 digits of pw
         last_3digit_table = self.document.add_table(rows=0, cols=1)
         last_3digit_table.style = 'Table Grid'
+        #set table font
+        font = last_3digit_table.style.font
+        font.name = 'Hind'
+        font.size = Pt(11)
         for i, val in enumerate(last_3digit):
             cells = last_3digit_table.add_row().cells
             cells[0].text = str(last_3digit[i])
@@ -134,6 +180,10 @@ class Reportgen(object):
         #eleventh table - last 4 digits of pw
         last_4digit_table = self.document.add_table(rows=0, cols=1)
         last_4digit_table.style = 'Table Grid'
+        #set table font
+        font = last_4digit_table.style.font
+        font.name = 'Hind'
+        font.size = Pt(11)
         for i, val in enumerate(last_4digit):
             cells = last_4digit_table.add_row().cells
             cells[0].text = str(last_4digit[i])
@@ -142,6 +192,10 @@ class Reportgen(object):
         #twelvth table - last 5 digits of pw
         last_5digit_table = self.document.add_table(rows=0, cols=1)
         last_5digit_table.style = 'Table Grid'
+        #set table font
+        font = last_5digit_table.style.font
+        font.name = 'Hind'
+        font.size = Pt(11)
         for i, val in enumerate(last_5digit):
             cells = last_5digit_table.add_row().cells
             cells[0].text = str(last_5digit[i])
@@ -150,6 +204,10 @@ class Reportgen(object):
         #thirteenth table - characterset block
         charset_table = self.document.add_table(rows=0, cols=1)
         charset_table.style = 'Table Grid'
+        #set table font
+        font = charset_table.style.font
+        font.name = 'Hind'
+        font.size = Pt(11)
         for i, val in enumerate(charset):
             cells = charset_table.add_row().cells
             cells[0].text = str(charset[i])
